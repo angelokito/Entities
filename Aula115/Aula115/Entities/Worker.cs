8bf2b5f3e91ea664d0a36256c1afdd5a2517c3cb
angelokito <kitochip7@gmail.com>
2020-07-01 12:08:22 -0300

    Novos arquivos deletados e novas classes adicionadas

---
+++ Aula115/Aula115/Entities/Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula115.Entities
+{
+    class Worker
+    {
+
+        public string Nome { get; set; }
+
+
+    }
+}
--- Aula115/Aula115/Program.cs
+++ Aula115/Aula115/Program.cs
@@ -8,21 +8,7 @@ namespace Aula115
     {
         static void Main(string[] args)
         {
-            Order order = new Order
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-
-            };
-            Console.WriteLine(order);
-
-            string txt = OrderStatus.PendingPayment.ToString();
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-            Console.WriteLine(os);
-
-            Console.WriteLine(txt);
-
+           
         }
     }
 }
