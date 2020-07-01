3ca2403d8d0f93da36ddb1bed912bafc7b763d3a
angelokito <kitochip7@gmail.com>
2020-07-01 12:36:02 -0300

    Adicionar ou remover contrato

--- Aula115/Aula115/Entities/Worker.cs
+++ Aula115/Aula115/Entities/Worker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Aula115.Entities.Enums;
 
 namespace Aula115.Entities
 {
@@ -8,7 +9,34 @@ namespace Aula115.Entities
     {
 
         public string Nome { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
 
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string nome, WorkerLevel level, double baseSalary, Department department)
+        {
+            Nome = nome;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+
+        }
 
     }
 }
