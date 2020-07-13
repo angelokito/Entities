16469560bfa90bace5a1fe7db5c63775d3bc2ec7
angelokito <kitochip7@gmail.com>
2020-07-13 10:54:36 -0300

    Classes e metodos prontos

--- Aula115/Aula115/Entities/Worker.cs
+++ Aula115/Aula115/Entities/Worker.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 using Aula115.Entities.Enums;
+using Aula115.Entities;
 
 namespace Aula115.Entities
 {
     class Worker
     {
 
-        public string Nome { get; set; }
-        public WorkerLevel Level { get; set; }
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; } //Propriedade WorkerLevel
         public double BaseSalary { get; set; }
-        public Department Department { get; set; }
+        public Department Department { get; set; } //Propriedade Department do tipo Department
 
         public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+        public static string name { get; internal set; }
 
-        public Worker()
+        public Worker() // Contrutor
         {
 
         }
 
-        public Worker(string nome, WorkerLevel level, double baseSalary, Department department)
+        public Worker(string nome, WorkerLevel level, double baseSalary, Department department)//Construtor com Argumentos
         {
-            Nome = nome;
+            Name = name;
             Level = level;
             BaseSalary = baseSalary;
             Department = department;
@@ -36,6 +38,19 @@ namespace Aula115.Entities
         {
             Contracts.Remove(contract);
 
+        }
+
+        public double Income (int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
         }
 
     }
--- Aula115/Aula115/Program.cs
+++ Aula115/Aula115/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Aula115.Entities;
 using System;
 using Aula115.Entities.Enums;
+using System.Globalization;
+
 
 namespace Aula115
 {
@@ -8,7 +10,50 @@ namespace Aula115
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Enter department 's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data: ");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many Contracts to this worker?  ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Data (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+
+            }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Enter month and year to calculate income (MM/YYYY):  ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year =  int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine("Nome: "  +worker.name) ;
+
+
+
+
         }
     }
 }
